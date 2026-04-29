57100fead5a147e69bed2f48d5ad0284261d3129
DelioCoder <davideveriwhere@gmail.com>
2026-04-29 14:50:53 -0500

    Added project fetch api in store reducer

--- redux/store.ts
+++ redux/store.ts
@@ -1,12 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
 import authReducer from './slices/authSlice';
+import { taskApi } from './services/task-api';
+import { projectApi } from './services/project-api';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    [taskApi.reducerPath]: taskApi.reducer,
+    [projectApi.reducerPath]: projectApi.reducer,
   },
-})
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([taskApi.middleware, projectApi.middleware])
+});
 
+setupListeners(store.dispatch);
 
 export type RootState = ReturnType<typeof store.getState>;
 
