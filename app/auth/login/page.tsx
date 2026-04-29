b0bf5af1f46f535dc37d2b40c1b0ed885907308e
DelioCoder <davideveriwhere@gmail.com>
2026-04-28 22:27:55 -0500

    initial page for login

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,29 @@
+export default function page() {
+    return (
+        <div className="min-h-dvh h-dvh w-full bg-zinc-100">
+            <div className="w-full h-full flex justify-center items-center">
+                <form className="card bg-zinc-50 w-96 shadow-xl p-4 rounded-lg">
+                    <div className="flex flex-col gap-y-4">
+                        <p className="text-zinc-800 font-semibold text-center">Iniciar Sesión</p>
+                        <div className="mb-2">
+                            <label className="block text-zinc-700 text-sm font-bold mb-2">
+                                Correo
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                        </div>
+
+                        <div className="mb-2">
+                            <label className="block text-zinc-700 text-sm font-bold mb-2">
+                                Contraseña
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                        </div>
+
+                        <button className="btn btn-primary">Neutral</button>
+
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
