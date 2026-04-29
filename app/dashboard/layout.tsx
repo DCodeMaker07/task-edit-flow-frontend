1fac67a01d6d064e08319ce4517d0be4480d3dbd
DelioCoder <davideveriwhere@gmail.com>
2026-04-29 18:56:32 -0500

    Updated: validating-user

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,18 @@
+"use client";
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/navigation';
 import { Sidebar } from '@/components'
-import React from 'react'
 
-export default function layout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default function LayoutPage({ children }: Readonly<{ children: React.ReactNode }>) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      router.push("/auth/login");
+    }
+  }, []);
   return (
     <div className='bg-zinc-100 lg:h-dvh w-full flex flex-col lg:flex-row gap-4 p-4'>
       <Sidebar />
