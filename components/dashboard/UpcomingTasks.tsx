458be20770fec8472dec6d8150c958e1e1d017ed
DelioCoder <davideveriwhere@gmail.com>
2026-04-29 15:12:47 -0500

    Updated task type import

--- components/dashboard/UpcomingTasks.tsx
+++ components/dashboard/UpcomingTasks.tsx
@@ -1,13 +1,16 @@
 import { mockTasks } from "@/utils/tasks"
 import { TaskItem } from "./TaskItem"
-
-export const UpcomingTasks = () => {
+import { Task } from "@/redux/services/interfaces/tasks/Task-response"
+type Props = {
+  tasks: Task[]
+}
+export const UpcomingTasks = ({ tasks }: Props) => {
   return (
-    <div className="w-full h-full bg-zinc-50 rounded-md shadow-md p-4">
+    <div className="w-full h-full bg-zinc-50 rounded-md shadow-md p-4 overflow-y-auto">
       <p className='text-xl text-zinc-800 font-semibold mb-6'>Upcoming Tasks</p>
       <div className="flex flex-col gap-4">
         {
-          mockTasks.map((item) => <TaskItem key={item.id} task={item} />)
+          tasks.map((item) => <TaskItem key={item.id} task={item} />)
         }
       </div>
     </div>
