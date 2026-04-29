0303896d423c5658d3f7136bd4a08817f6d52976
DelioCoder <davideveriwhere@gmail.com>
2026-04-29 15:24:22 -0500

    Added projectFormModal initial view

---
+++ components/projects/ProjectFormModal.tsx
@@ -0,0 +1,13 @@
+export const ProjectFormModal = () => {
+    return (
+        <dialog id="my_project_from_modal" className="modal">
+            <div className="modal-box">
+                <h3 className="font-bold text-lg">Hello!</h3>
+                <p className="py-4">Press ESC key or click outside to close</p>
+            </div>
+            <form method="dialog" className="modal-backdrop">
+                <button>close</button>
+            </form>
+        </dialog>
+    )
+}
