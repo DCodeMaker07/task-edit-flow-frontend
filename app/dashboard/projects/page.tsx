a2cdebd3ace426d6e73928910ed136191c451ca7
DelioCoder <davideveriwhere@gmail.com>
2026-04-29 11:06:03 -0500

    Added mockProjects in view

--- app/dashboard/projects/page.tsx
+++ app/dashboard/projects/page.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
-
+import { ProjectItem } from '@/components'
+import { mockProjects } from '@/utils/projects'
 export default function ProjectsPage() {
   return (
-    <div>ProjectsPage</div>
+    <div className='lg:w-[88%] flex flex-col gap-6'>
+      <div className='flex justify-between'>
+        <p className='text-zinc-500'>Manage your projects and track progress</p>
+        <button className="btn btn-primary">
+          <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24">
+            <path fill="currentColor" d="M11 13H5v-2h6V5h2v6h6v2h-6v6h-2z"></path>
+          </svg>
+          New Project
+        </button>
+      </div>
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-6'>
+        {
+          mockProjects.map((item) => <ProjectItem key={item.id} project={item} />)
+        }
+      </div>
+
+    </div>
   )
 }
