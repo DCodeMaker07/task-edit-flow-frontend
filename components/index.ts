12b71a7b343b4a596667c19ae8a7b78fad229351
DelioCoder <davideveriwhere@gmail.com>
2026-04-29 15:24:12 -0500

    Added: projectFormModal

--- components/index.ts
+++ components/index.ts
@@ -6,6 +6,7 @@ export * from './dashboard/StatCard';
 export * from './dashboard/StatsGrid';
 
 export * from './projects/ProjectItem';
+export * from './projects/ProjectFormModal';
 
 export * from './tasks/TaskFilters';
 export * from './tasks/TaskTable';
