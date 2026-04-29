3d80283fa7af4a7d0f179313f7e8e782bf2644b0
DelioCoder <davideveriwhere@gmail.com>
2026-04-29 11:05:45 -0500

    Added UpcomingTasks component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,9 @@
+import { UpcomingTasks } from "@/components";
+
 export default function page() {
   return (
-    <div className='lg:w-[88%]'>
-      <div className='grid grid-cols-4 gap-10'>
+    <div className='lg:w-[88%] flex flex-col gap-6'>
+      <div className='grid grid-cols-1 lg:grid-cols-4 gap-10'>
         <div className='bg-zinc-50 shadow-md p-8 rounded-md'>
           <div className='flex flex-col gap-y-4'>
             <div className='flex flex-row justify-between'>
@@ -18,7 +20,7 @@ export default function page() {
           <div className='flex flex-col gap-y-4'>
             <div className='flex flex-row justify-between'>
               <p className='text-zinc-700'>In Progress</p>
-              <p className='bg-blue-200 rounded-full px-2'>5</p>
+              <p className='bg-blue-200 text-blue-500 rounded-full px-2'>5</p>
             </div>
             <div>
               <p className='text-3xl'>5</p>
@@ -30,7 +32,7 @@ export default function page() {
           <div className='flex flex-col gap-y-4'>
             <div className='flex flex-row justify-between'>
               <p className='text-zinc-700'>In Progress</p>
-              <p className='bg-yellow-200 rounded-full px-2'>3</p>
+              <p className='bg-yellow-200 text-yellow-500 rounded-full px-2'>3</p>
             </div>
             <div>
               <p className='text-3xl'>3</p>
@@ -42,7 +44,7 @@ export default function page() {
           <div className='flex flex-col gap-y-4'>
             <div className='flex flex-row justify-between'>
               <p className='text-zinc-700'>In Progress</p>
-              <p className='bg-yellow-200 rounded-full px-2'>28</p>
+              <p className='bg-green-200 text-green-500 rounded-full px-2'>28</p>
             </div>
             <div>
               <p className='text-3xl'>28</p>
@@ -50,6 +52,8 @@ export default function page() {
           </div>
         </div>
       </div>
+
+      <UpcomingTasks />
     </div>
   )
 }
