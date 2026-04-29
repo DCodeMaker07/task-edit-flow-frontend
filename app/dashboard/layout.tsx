502844902b8cb9890344ddeb5dff8b895b089d8a
DelioCoder <davideveriwhere@gmail.com>
2026-04-29 16:12:22 -0500

    Updated: responsive height

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function layout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className='bg-zinc-100 h-dvh w-full flex flex-col lg:flex-row gap-4 p-4'>
+    <div className='bg-zinc-100 lg:h-dvh w-full flex flex-col lg:flex-row gap-4 p-4'>
       <Sidebar />
       {children}
     </div>
