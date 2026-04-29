a7220214f00a73a2c75f37ab0d42f0c228fc1dd7
DelioCoder <davideveriwhere@gmail.com>
2026-04-29 11:09:37 -0500

    Added initial view for TaskCardComponent

--- components/dashboard/UpcomingTasks.tsx
+++ components/dashboard/UpcomingTasks.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { mockTasks } from "@/utils/tasks"
+import { TaskItem } from "./TaskItem"
 
 export const UpcomingTasks = () => {
   return (
-    <div>UpcomingTasks</div>
+    <div className="w-full h-full bg-zinc-50 rounded-md shadow-md p-4">
+      <p className='text-xl text-zinc-800 font-semibold mb-6'>Upcoming Tasks</p>
+      <div className="flex flex-col gap-4">
+        {
+          mockTasks.map((item) => <TaskItem key={item.id} task={item} />)
+        }
+      </div>
+    </div>
   )
 }
