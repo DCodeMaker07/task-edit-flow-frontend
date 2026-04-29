f9ef249e98003ff36047c473536ddf963ffb876b
DelioCoder <davideveriwhere@gmail.com>
2026-04-29 09:18:37 -0500

    Added initial components for dashboard

---
+++ components/dashboard/StatCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const StatCard = () => {
+  return (
+    <div>StatCard</div>
+  )
+}
---
+++ components/dashboard/UpcomingTasks.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const UpcomingTasks = () => {
+  return (
+    <div>UpcomingTasks</div>
+  )
+}
