3d58a95266aeedf7452666a81e6a04bcdc5b4ac6
DelioCoder <davideveriwhere@gmail.com>
2026-04-29 14:48:38 -0500

    Added new components from dashboard

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,10 @@ export * from './layout/Header';
 export * from './layout/Sidebar';
 
 export * from './dashboard/UpcomingTasks';
+export * from './dashboard/StatCard';
+export * from './dashboard/StatsGrid';
 
 export * from './projects/ProjectItem';
+
+export * from './tasks/TaskFilters';
+export * from './tasks/TaskTable';
