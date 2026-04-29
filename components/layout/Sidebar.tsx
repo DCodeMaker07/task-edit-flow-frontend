6871797692ad927fddc9b604dc2511581fc7d977
DelioCoder <davideveriwhere@gmail.com>
2026-04-29 11:08:10 -0500

    Refactor sidebar items as an object

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,8 +1,35 @@
 "use client"
-import { useAppSelector } from '@/redux/hooks'
 import Image from 'next/image'
+import { usePathname } from 'next/navigation'
+import { useAppSelector } from '@/redux/hooks'
+import Link from 'next/link';
+
+const menuItems = [
+  {
+    label: 'Home',
+    path: '/dashboard',
+    icon: (
+      <path d="M13 9V3h8v6zM3 13V3h8v10zm10 8V11h8v10zM3 21v-6h8v6z" />
+    ),
+  },
+  {
+    label: 'Projects',
+    path: '/dashboard/projects',
+    icon: (
+      <path d="M16 12v-2h2v2zm0 2h-2v-2h2zm0 2v-2h2v2zm-4.825-8l-2-2H4v12h10v-2h2v2h4V8h-4v2h-2V8zM2 20V4h8l2 2h10v14zm2-2V6z" />
+    ),
+  },
+  {
+    label: 'Tasks',
+    path: '/dashboard/tasks',
+    icon: (
+      <path d="M5.55 19L2 15.45l1.4-1.4l2.125 2.125l4.25-4.25l1.4 1.425zm0-8L2 7.45l1.4-1.4l2.125 2.125l4.25-4.25l1.4 1.425zM13 17v-2h9v2zm0-8V7h9v2z" />
+    ),
+  },
+];
 
 export const Sidebar = () => {
+  const pathname = usePathname();
   const user = useAppSelector(state => state.auth.user);
   return (
     <div className='w-[12%] bg-zinc-50 rounded-md shadow p-4'>
@@ -11,64 +38,37 @@ export const Sidebar = () => {
           <div className='mt-2 mb-8'>
             <p className='text-2xl font-bold tracking-wider'>TaskFlow</p>
           </div>
-          <div className='flex lg:flex-col gap-4'>
-            {/* HOME */}
-            <div className='flex gap-2 group transition-all duration-500 cursor-pointer hover:bg-neutral rounded-md hover:py-2 hover:pl-2'>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width={24}
-                height={24}
-                viewBox="0 0 24 24"
-                className="text-zinc-700 group-hover:text-zinc-50 transition-all"
-              >
-                <path
-                  fill="currentColor"
-                  d="M13 9V3h8v6zM3 13V3h8v10zm10 8V11h8v10zM3 21v-6h8v6z"
-                />
-              </svg>
-              <p className='tracking-wider text-zinc-700 group-hover:text-zinc-50 transition-all'>
-                Home
-              </p>
-            </div>
 
-            {/* PROJECTS */}
-            <div className='flex gap-2 group transition-all duration-500 cursor-pointer hover:bg-neutral rounded-md hover:py-2 hover:pl-2'>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width={24}
-                height={24}
-                viewBox="0 0 24 24"
-                className="text-zinc-700 group-hover:text-zinc-50 transition-all"
-              >
-                <path
-                  fill="currentColor"
-                  d="M16 12v-2h2v2zm0 2h-2v-2h2zm0 2v-2h2v2zm-4.825-8l-2-2H4v12h10v-2h2v2h4V8h-4v2h-2V8zM2 20V4h8l2 2h10v14zm2-2V6z"
-                />
-              </svg>
-              <p className='tracking-wider text-zinc-700 group-hover:text-zinc-50 transition-all'>
-                Projects
-              </p>
-            </div>
+          <div className="flex lg:flex-col gap-4">
+            {menuItems.map((item) => {
+              const isActive = pathname === item.path;
 
-            {/* TASKS */}
-            <div className='flex gap-2 group transition-all duration-500 cursor-pointer hover:bg-neutral rounded-md hover:py-2 hover:pl-2'>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width={24}
-                height={24}
-                viewBox="0 0 24 24"
-                className="text-zinc-700 group-hover:text-zinc-50 transition-all"
-              >
-                <path
-                  fill="currentColor"
-                  d="M5.55 19L2 15.45l1.4-1.4l2.125 2.125l4.25-4.25l1.4 1.425zm0-8L2 7.45l1.4-1.4l2.125 2.125l4.25-4.25l1.4 1.425zM13 17v-2h9v2zm0-8V7h9v2z"
-                />
-              </svg>
-              <p className='tracking-wider text-zinc-700 group-hover:text-zinc-50 transition-all'>
-                Tasks
-              </p>
-            </div>
+              return (
+                <Link
+                  key={item.path}
+                  href={item.path}
+                  className={`flex gap-2 group cursor-pointer rounded-md transition-all duration-500 ${isActive ? 'bg-neutral py-2 pl-2' : 'hover:bg-neutral hover:py-2 hover:pl-2'} `}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={24}
+                    height={24}
+                    viewBox="0 0 24 24"
+                    className={`transition-all ${isActive ? 'text-zinc-50' : 'text-zinc-700 group-hover:text-zinc-50'}`}
+                  >
+                    <g fill="currentColor">{item.icon}</g>
+                  </svg>
+
+                  <p
+                    className={`tracking-wider transition-all ${isActive ? 'text-zinc-50' : 'text-zinc-700 group-hover:text-zinc-50'}`}
+                  >
+                    {item.label}
+                  </p>
+                </Link>
+              );
+            })}
           </div>
+
         </div>
 
         <div>
