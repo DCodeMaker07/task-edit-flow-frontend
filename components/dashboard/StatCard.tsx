8e78eb9e0c3792e4395e951262bcfee67a2e575a
DelioCoder <davideveriwhere@gmail.com>
2026-04-29 14:48:54 -0500

    Added initial view for StatCard

--- components/dashboard/StatCard.tsx
+++ components/dashboard/StatCard.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+type Props = {
+  status: string;
+  label: string;
+  count: number;
+  style: string;
+};
 
-export const StatCard = () => {
+export const StatCard = ({ label, count, style }: Props) => {
   return (
-    <div>StatCard</div>
-  )
-}
+    <div className='bg-zinc-50 shadow-md p-8 rounded-md'>
+      <div className='flex flex-col gap-y-4'>
+        
+        <div className='flex justify-between'>
+          <p className='text-zinc-700'>{label}</p>
+          <p className={`rounded-full px-2 ${style}`}>
+            {count}
+          </p>
+        </div>
+
+        <div>
+          <p className='text-3xl'>{count}</p>
+        </div>
+
+      </div>
+    </div>
+  );
+};
